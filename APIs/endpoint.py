4e52dba8c4dee0d5037133b6e4ea212d619c8113
GiorgioMannarini <giorgio.mannarini@gmail.com>
2021-11-05 14:24:49 +0100

    Thank you co-pilot

--- APIs/endpoint.py
+++ APIs/endpoint.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, jsonify
-
+import pandas as pd
 from model.country_data import CountryData
 from model.predict import Predict
 from flask_cors import CORS
@@ -24,6 +24,18 @@ def get_constant_data(country):
     cd = CountryData()
     return jsonify(cd.get_constant_features(country))
 
+@app.route('/api/v1/<country>/get_variable_data', methods=['GET'])
+def get_variable_data(country):
+    parameters = request.json
+    cd = CountryData()
+    policies_df = cd.get_policies_for_a_period(country, parameters['start_date'], parameters['end_date'])
+    #convert the index, which is a date, into a string
+    policies_df.index = policies_df.index.astype(str)
+    format_to_return = {"dates": policies_df.index.values.tolist(), "policies": {}}
+    for policy in policies_df.columns:
+        format_to_return["policies"][policy] = policies_df[policy].values.tolist()
+
+    return jsonify(format_to_return)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
--- model/country_data.py
+++ model/country_data.py
@@ -12,6 +12,9 @@ class CountryData:
     def get_constant_features(self, iso_code: str):
         return self.data[self.data.iso_code == iso_code][self.features['constant']].iloc[0].to_dict()
 
+    def get_policies_name(self):
+        return self.features['variable'][12:]
+
     @staticmethod
     def extract_feature_names():
         with open('../model/config/r_estim_features.yaml', 'r', encoding='utf-8') as file:
@@ -23,3 +26,17 @@ class CountryData:
                             swissre_features['policies']
 
         return {'constant': constant_columns, 'variable': variable_columns}
+
+    def get_policies_for_a_period(self, iso_code: str, start_date: str, end_date: str):
+      # call the function get_sundays to get the sundays of the given period
+      sundays = self.get_sundays_between_dates(start_date, end_date)
+      # get the data of the given country where the index is equal to the sundays
+      data = self.data[(self.data.iso_code == iso_code) & (self.data.index.isin(sundays))][self.get_policies_name()]
+      # return the dataframe
+      return data
+
+    def get_sundays_between_dates(self, start_date: str, end_date: str):
+        start_date = pd.to_datetime(start_date)
+        end_date = pd.to_datetime(end_date)
+        sundays = pd.date_range(start_date, end_date, freq='W-SUN')
+        return sundays
